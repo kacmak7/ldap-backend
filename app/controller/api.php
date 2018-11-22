a1bb0e6807970c6b88b1364c88f87d624d3dce62
Kacper Makuch <kacbull007@gmail.com>
2018-11-22 15:40:21 +0100

    Add test endpoints and enable retrieving all users

--- app/controller/api.php
+++ app/controller/api.php
@@ -11,28 +11,51 @@ namespace Controller;
  */
 class Api {
 
+	
 
-    public function GET_dictionary_systems(\Base $f3) {
-        $domain = array(
-            'marol.com.pl',
-            'chatapolska.pl',
-        );
+	public function GET_dictionary_systems(\Base $f3) {
+		$domain = array(	
+			'marol.com.pl',
+			'chatapolska.pl',
+		);
 
-        echo $this->_view->render('json_data.phtml', 'application/json', array('data' => $domains));
-    }
+		echo $this->_view->render('json_data.phtml', 'application/json', array('data' => $domains));
+	}
 
-    public function GET_test_message(\Base $f3) {
-	    $testMessage = 'papaya';
+	public function GET_test_message(\Base $f3) {
+		$testMessage = 'papaya';
 
+		$view=\View::instance();
+		echo $view->render('json_data.phtml', 'application/json', array('data' => $testMessage));
+	}
 
-	    $view=\View::instance();
-	    echo $view->render('json_data.phtml', 'application/json', array('data' => $testMessage));
-    }
+	public function GET_test_numbers(\Base $f3) {
+		$data = array(1, 5, 2, 97, 26);
 
-    public function GET_test_numbers(\Base $f3) {
-	    $data = array(1, 5, 2, 97, 26);
+		$view=\View::instance();
+		echo $view->render('json_data.phtml', 'application_json', array('data' => $data));
+	}	
 
-	    $view=\View::instance();
-	    echo $view->render('json_data.phtml', 'application_json', array('data' => $data));
-    }
+	public function GET_users(\Base $d3) {
+		$ldap_dn = 'cn=admin,dc=marol,dc=com,dc=pl';
+		$ldap_password = 'secret';
+		$ldap_con = ldap_connect('127.0.0.1');
+		ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+		if (ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
+			echo "Bind successful";
+			$filter = "(uid=*)";
+			$result = ldap_search($ldap_con, "dc=marol,dc=com,dc=pl", $filter) or exit("search error");
+			$entries = ldap_get_entries($ldap_con, $result);
+			print "<pre>";
+			print_r ($entries);$f3->route('GET /api/te/@no',
+	function($f3, $params) {
+		echo "parameter: ";
+		echo $params['no'];
+	});
+			print "</pre>";
+		}
+		else {
+			echo "ErRoR";
+		}
+	}
 }
--- public/index.php
+++ public/index.php
@@ -20,7 +20,117 @@ $f3->route('GET /api/test',
 	function() {
 		echo 'test';
 	});
-$f3->route('GET /api/test/con', 'Controller\Api->GET_test_message');
+$f3->route('GET /api/test1', 'Controller\Api->GET_test_message');
 $f3->route('GET /api/test/array', 'Controller\Api->GET_test_numbers');
-$f3->run();
 
+$f3->route('GET /api/users', 'Controller\Api->GET_users');
+
+$f3->route('GET /api/bind', 
+	function() {
+		$ldap_dn = "cn=admin,dc=marol,dc=com,dc=pl";
+		$ldap_password = "secret";
+		$ldap_con = ldap_connect("127.0.0.1");
+
+		ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+		if (ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
+			echo "Bind successful";
+
+
+		}
+		else {
+			echo "ErRoR";
+		}
+	});
+$f3->route('GET /api/search', 
+	function() {
+		$ldap_dn = "cn=admin,dc=marol,dc=com,dc=pl";
+		$ldap_password = "secret";
+		$ldap_con = ldap_connect("127.0.0.1");
+
+		ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+		if (ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
+			echo "Bind successful";
+
+			$filter = "(uid=andrzejk)";
+			$result = ldap_search($ldap_con, "dc=marol,dc=com,dc=pl", $filter) or exit("search error");
+			$entries = ldap_get_entries($ldap_con, $result);
+
+			print "<pre>";
+			print_r ($entries);
+			print "</pre>";
+
+
+		}
+		else {
+			echo "ErRoR";
+		}
+	});
+
+$f3->route('GET /api/add', 
+	function() {
+		$ldap_dn = "cn=admin,dc=marol,dc=com,dc=pl";
+		$ldap_password = "secret";
+		$ldap_con = ldap_connect("127.0.0.1");
+
+		ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+		if (ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
+			echo "Bind successful";
+
+			$info["objectclass"][0] = "inetOrgPerson";
+			$info["objectclass"][1] = "person";
+			$info["objectclass"][2] = "organizationalPerson";
+			$info["objectclass"][3] = "posixAccount";
+			$info["objectclass"][4] = "shadowAccount";
+			$info["objectclass"][5] = "top";
+			$info["cn"] = "Tasty Test";
+			$info["gidNumber"] = "99999";
+			$info["homeDirectory"] = "/home/testt1";
+			$info["sn"] = "Test";
+			$info["uid"] = "testt1";
+			$info["uidNumber"] = "99999";
+
+			ldap_add($ldap_con, "uid=testt1,ou=users,dc=marol,dc=com,dc=pl", $info);
+			ldap_close($ldap_con);
+
+		}
+		else {
+			echo "ErRoR";
+		}
+	});
+
+$f3->route('GET /api/adldap2',
+	function() {
+		$ad = new Adldap\Adldap();
+
+		$config = [
+			'hosts' => ['marol.com.pl', '127.0.0.1'],
+			'base_dn' => 'dc=marol,dc=com,dc=pl',
+			'username' => 'admin',
+			'password' => 'secret',
+		];
+		$ad->addProvider($config);
+
+		try {
+			$provider = $ad->connect();
+
+			//$results = $provider->search()->where('cn', '=', 'Dariusz Kowalczyk')->get();
+			//echo $results;
+		} catch (Adldap\Auth\BindException $e) {
+			echo "erroR";
+		}
+	});
+$f3->route('GET /api/te/@no',
+	function($f3, $params) {
+		echo "parameter: ";
+		echo $params['no'];
+	});
+
+$f3->route('GET /api/te',
+	function($f3, $params) {
+		echo 'no parameters';
+	});
+
+$f3->run();
