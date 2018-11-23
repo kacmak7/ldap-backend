c601786eb76697574ccc92868e8807989a7b36a2
Charlie Root <root@bowen.zipper.release11.com>
2018-11-23 15:14:08 +0100

    Build

---
+++ app/model/InetOrgPersonMapper.php
@@ -0,0 +1,25 @@
+<?php
+
+class InetOrgPersonMapper {
+
+	const MIN_UID_NUMBER = 16384;
+
+	public function __construct() {
+		$this->_ldap_dn = 'cn=admin,dc=marol,dc=com,dc=pl';
+		$this->_ldap_password = 'secret';
+		$this->_ldap_con = ldap_connect('127.0.0.1');
+	}
+
+	public function fetch() {
+		ldap_set_option($this->_ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+		if (ldap_bind($this->_ldap_con, $this->_ldap_dn, $this->_ldap_password)) {
+			$filter = "(uid=*)";
+			$result = ldap_search($this->_ldap_con, "dc=marol,dc=com,dc=pl", $filter) or exit("search error");
+			$entries = ldap_get_entries($this->_ldap_con, $result);
+			return $entries;
+		}
+		else {
+			return array('error');
+		};
+	}
+}
