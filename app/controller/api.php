c1b9ba2dd81274f3171956631accc264ee1eb438
Kacper Makuch <kacbull007@gmail.com>
2018-11-26 16:12:19 +0100

    Build_1

--- app/controller/api.php
+++ app/controller/api.php
@@ -1,9 +1,5 @@
 <?php
-
-/*
- * $Id$
- */
-
+	
 namespace Controller;
 
 /**
@@ -11,7 +7,23 @@ namespace Controller;
  */
 class Api {
 
-	
+	private $_view;
+	private $_ldap_dn;
+	private $_ldap_password;
+	private $_ldap_con;
+
+	/**
+	 * @var \Model\InetOrgPersonMapper
+	 */
+	private $_inetOrgPersonMapper;
+
+	public function __construct() {
+		$this->_view = \View::instance();
+		/*$this->_ldap_dn = 'cn=admin,dc=marol,dc=com,dc=pl';
+		$this->_ldap_password = 'secret';
+		$this->_ldap_con = ldap_connect('127.0.0.1');*/
+		$this->_inetOrgPersonMapper = new \Model\InetOrgPersonMapper();
+	}
 
 	public function GET_dictionary_systems(\Base $f3) {
 		$domain = array(	
@@ -36,26 +48,22 @@ class Api {
 		echo $view->render('json_data.phtml', 'application_json', array('data' => $data));
 	}	
 
-	public function GET_users(\Base $d3) {
-		$ldap_dn = 'cn=admin,dc=marol,dc=com,dc=pl';
-		$ldap_password = 'secret';
-		$ldap_con = ldap_connect('127.0.0.1');
-		ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
-		if (ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
-			echo "Bind successful";
-			$filter = "(uid=*)";
-			$result = ldap_search($ldap_con, "dc=marol,dc=com,dc=pl", $filter) or exit("search error");
-			$entries = ldap_get_entries($ldap_con, $result);
-			print "<pre>";
-			print_r ($entries);$f3->route('GET /api/te/@no',
-	function($f3, $params) {
-		echo "parameter: ";
-		echo $params['no'];
-	});
-			print "</pre>";
+	public function GET_users(\Base $f3) {
+		echo $this->_view->render('json_data.phtml', 'application_json', array('data' => $this->_inetOrgPersonMapper->fetch('*')));
+	}
+
+	public function POST_user(\Base $f3) {
+		ldap_set_option($this->_ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+		if(ldap_bind($this->_ldap_con, $this->_ldap_dn, $this->_ldap_password)) {
+			$info["objectclass"][0] = "inetOrgPerson";
+			$info["objectclass"][1] = "person";
+			$info["objectclass"][2] = "organizationalPerson";
+			$info["objectclass"][3] = "posixAccount";
+			$info["objectclass"][4] = "shadowAccount";
+			$info["objectclass"][5] = "top";
 		}
-		else {
-			echo "ErRoR";
-		}
+
+		echo $this->_view->render('json_data.phtml', 'application_json', array('data' => $this->_inetOrgPersonMapper->fetch('testt')));
 	}
 }
